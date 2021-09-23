4ac665c613d0a8c0b37da40632ff90fc037e056e
catsbi <killfine1988@gmail.com>
2021-09-23 13:01:32 +0900

    feat: Task 도메인 테스트 케이스 작성

--- app/src/main/java/com/codesoom/assignment/todo/domain/Task.java
+++ app/src/main/java/com/codesoom/assignment/todo/domain/Task.java
@@ -1,20 +1,44 @@
 package com.codesoom.assignment.todo.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
 
 /**
  * 할 일 정보
  */
 @Entity
 @Getter
+@NoArgsConstructor
 public class Task {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
+
+    public Task(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Task)) {
+            return false;
+        }
+        Task task = (Task) o;
+        return Objects.equals(id, task.id) && Objects.equals(title, task.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title);
+    }
 }
---
+++ app/src/test/java/com/codesoom/assignment/todo/domain/TaskTest.java
@@ -0,0 +1,115 @@
+package com.codesoom.assignment.todo.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayName("Task 클래스")
+class TaskTest {
+
+    @Nested
+    @DisplayName("생성자는")
+    class Describe_constructor {
+        @DisplayName("인자가 정상적인 경우, Task 객체가 생성된다.")
+        @ParameterizedTest
+        @ValueSource(strings = {"test", "test1", "catsbi", "crong", "밥 먹고, 공부하기", "복습한 뒤 포스팅하기",
+                "밥먹고 공부하다가, 모르는 내용은 구글링 해본 뒤, 톡방에 물어보고 그래도 답이 안나오면 리뷰어분들에게 요청해본 뒤 정리"})
+        void createTaskWithValidTitle(String title) {
+            final Task newTask = new Task(title);
+
+            assertThat(newTask.getTitle()).isEqualTo(title);
+            assertThat(newTask).isEqualTo(new Task(title));
+        }
+
+        @DisplayName("인자가 비정상인 경우, 예외를 던진다.")
+        @ParameterizedTest
+        @NullAndEmptySource
+        void createTaskWithInvalidTitle(String invalidTitle) {
+            assertThatThrownBy(() -> new Task(invalidTitle))
+                    .isInstanceOf(InvalidTaskTitleException.class);
+        }
+    }
+
+    @Nested
+    @DisplayName("from 메서드는")
+    class Describe_from {
+        @DisplayName("인자가 정상적인 경우, Task 객체가 생성된다.")
+        @ParameterizedTest
+        @ValueSource(strings = {"test", "test1", "catsbi", "crong", "밥 먹고, 공부하기", "복습한 뒤 포스팅하기",
+                "밥먹고 공부하다가, 모르는 내용은 구글링 해본 뒤, 톡방에 물어보고 그래도 답이 안나오면 리뷰어분들에게 요청해본 뒤 정리"})
+        void createTaskWithValidTitle(String title) {
+            final Task newTask = Task.from(title);
+
+            assertThat(newTask.getTitle()).isEqualTo(title);
+            assertThat(newTask).isEqualTo(Task.from(title));
+        }
+
+        @DisplayName("인자가 비정상인 경우, 예외를 던진다.")
+        @ParameterizedTest
+        @NullAndEmptySource
+        void createTaskWithInvalidTitle(String invalidTitle) {
+            assertThatThrownBy(() -> new Task(invalidTitle))
+                    .isInstanceOf(InvalidTaskTitleException.class);
+        }
+    }
+
+    @Nested
+    @DisplayName("update 메서드는")
+    class Describe_update {
+        private static final String TITLE = "밥먹기";
+
+        private Task originTask;
+
+        private List<Task> validTasks;
+        private Task invalidTask;
+
+        @BeforeEach
+        void setUp() {
+            originTask = Task.from(TITLE);
+
+            validTasks = Arrays.asList(
+                    Task.from("test"),
+                    Task.from("test1"),
+                    Task.from("catsbi"),
+                    Task.from("crong"),
+                    Task.from("밥 먹고, 공부하기"),
+                    Task.from("복습한 뒤 포스팅하기"),
+                    Task.from("밥먹고 공부하다가, 모르는 내용은 구글링 해본 뒤, 톡방에 물어보고 그래도 답이 안나오면 리뷰어분들에게 요청해본 뒤 정리")
+            );
+
+            invalidTask = new Task();
+        }
+
+        @DisplayName("인자가 유효하면, 업데이트를 수행한다.")
+        @Test
+        void updateTaskWithValidTitle() {
+            for (Task validTask : validTasks) {
+                originTask.update(validTask);
+
+                assertThat(originTask.getTitle()).isEqualTo(validTask.getTitle());
+            }
+        }
+
+        @DisplayName("인자가 유효하지 않으면, 예외를 던진다")
+        @ParameterizedTest
+        @NullAndEmptySource
+        void updateTaskWithInvalidTitle(String invalidTitle) {
+            ReflectionTestUtils.setField(invalidTask, "title", invalidTitle);
+
+            assertThatThrownBy(()-> originTask.update(invalidTask))
+                    .isInstanceOf(InvalidTaskTitleException.class);
+        }
+    }
+
+}
