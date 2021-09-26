a3fcef00f1c2cf67f1007de47ba78fe362960f28
catsbi <killfine1988@gmail.com>
2021-09-26 17:03:53 +0900

    feat: TaskController 주석 작성

--- app/src/main/java/com/codesoom/assignment/todo/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/todo/controllers/TaskController.java
@@ -3,6 +3,8 @@ package com.codesoom.assignment.todo.controllers;
 import com.codesoom.assignment.todo.application.TaskService;
 import com.codesoom.assignment.todo.domain.Task;
 import com.codesoom.assignment.todo.dto.TaskData;
+import com.codesoom.assignment.todo.errors.InvalidTaskTitleException;
+import com.codesoom.assignment.todo.errors.TaskNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -30,6 +32,11 @@ public class TaskController {
     private final TaskService taskService;
 
 
+    /**
+     * 모든 할 일 목록을 조회한다.
+     *
+     * @return 할 일 목록
+     */
     @GetMapping
     public List<TaskData> findAllTask() {
         final List<Task> foundTasks = taskService.findAllTasks();
@@ -43,6 +50,13 @@ public class TaskController {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * 할 일을 조회한다.
+     *
+     * @param id 할 일 식별자
+     * @return 할 일 상세정보
+     * @throws TaskNotFoundException 할 일을 찾을 수 없는 경우
+     */
     @GetMapping("{id}")
     public TaskData findTask(@PathVariable long id) {
         final Task foundTask = taskService.findTask(id);
@@ -50,6 +64,13 @@ public class TaskController {
         return TaskData.of(foundTask);
     }
 
+    /**
+     * 할 일을 등록한다.
+     *
+     * @param taskData 할 일 정보
+     * @return 등록된 할 일 정보
+     * @throws InvalidTaskTitleException 할 일 정보가 유효하지 않은 경우
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public TaskData createTask(@RequestBody TaskData taskData) {
@@ -58,12 +79,27 @@ public class TaskController {
         return TaskData.of(savedTask);
     }
 
+    /**
+     * 할 일을 수정한다.
+     *
+     * @param id 수정 할 할일 식별자
+     * @param taskData 할 일
+     * @return 수정된 할 일 정보
+     * @throws TaskNotFoundException 할 일을 찾을 수 없는 경우
+     * @throws InvalidTaskTitleException 할 일 정보가 유효하지 않은 경우
+     */
     @RequestMapping(path = "{id}", method = {RequestMethod.PUT, RequestMethod.PATCH})
     public TaskData updateTask(@PathVariable long id, @RequestBody TaskData taskData) {
         final Task updatedTask = taskService.updateTask(id, taskData);
         return TaskData.of(updatedTask);
     }
 
+    /**
+     * 할 일을 삭제한다.
+     *
+     * @param id 삭제 할 할 일 식별자
+     * @throws TaskNotFoundException 할 일을 찾을 수 없는 경우
+     */
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteTask(@PathVariable long id) {
